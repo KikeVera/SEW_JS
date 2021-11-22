312e9bebebecdb03e63f636aa24bbac0eaa92a3d
Enrique Vera Cueto <UO246711@uniovi.es>
2021-11-22 02:46:08 +0100

    Mas bugs corregidos zzzzz

--- Ejercicio-07/Ejercicio7.js
+++ Ejercicio-07/Ejercicio7.js
@@ -21,14 +21,14 @@ class RasmusJS{
 	
 	codificar(){
 		
-		$("p:eq(0)").html("<p>43924dd78738d2af7ce01a5f3bf6de66</p>");
-		$("p:eq(2)").html("<p>e879d0d9acb48aaa647eff1ce79dd734</p>");
+		$("p:eq(0)").html("43924dd78738d2af7ce01a5f3bf6de66");
+		$("p:eq(1)").html("e879d0d9acb48aaa647eff1ce79dd734");
 		
 	}
 	
 	descodificar(){
-		$("p:eq(0)").html("<p>PHP es un lenguaje de programación de uso general que se adapta especialmente al desarrollo web​ Fue creado inicialmente por el programador danés-canadiense Rasmus Lerdorf en 1994​ En la actualidad, la implementación de referencia de PHP es producida por The PHP Group.</p>");
-		$("p:eq(2)").html("<p>Rasmus Lerdorf (nacido el 22 de noviembre de 1968) es un programador danés-canadiense. Fue coautor e inspiró el lenguaje de scripting PHP, también fue autor de las dos primeras versiones del lenguaje y participó en el desarrollo de versiones posteriores dirigidas por un grupo de desarrolladores que incluía a Jim Winstead (quien más tarde creó blogs), Stig Bakken, Shane Caraveo , Andi Gutmans y Zeev Suraski. Continúa contribuyendo al proyecto.</p>");
+		$("p:eq(0)").html("PHP es un lenguaje de programación de uso general que se adapta especialmente al desarrollo web​ Fue creado inicialmente por el programador danés-canadiense Rasmus Lerdorf en 1994​ En la actualidad, la implementación de referencia de PHP es producida por The PHP Group.");
+		$("p:eq(1)").html("Rasmus Lerdorf (nacido el 22 de noviembre de 1968) es un programador danés-canadiense. Fue coautor e inspiró el lenguaje de scripting PHP, también fue autor de las dos primeras versiones del lenguaje y participó en el desarrollo de versiones posteriores dirigidas por un grupo de desarrolladores que incluía a Jim Winstead (quien más tarde creó blogs), Stig Bakken, Shane Caraveo , Andi Gutmans y Zeev Suraski. Continúa contribuyendo al proyecto");
 		
 	}
 	
